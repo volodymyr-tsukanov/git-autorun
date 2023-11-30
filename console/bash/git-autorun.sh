3c3d0e16f3727a2beffd75f8496345cefa246cb1
Volodymyr <134626794+volodymyr-tsukanov@users.noreply.github.com>
2023-11-30 11:55:58 +0100

    created functions run, initialize, awake, printTab, welcomePrint

--- console/bash/git-autorun.sh
+++ console/bash/git-autorun.sh
@@ -1,3 +1,54 @@
 #!/bin/bash
 
-echo test
+# Variables
+welcomeText="/---/ Git autorun \\---\\"
+
+TAB_SIZE=4
+ROWS=64
+COLS=64
+
+
+# Methods
+initialize(){
+	ROWS=$(tput lines)
+	COLS=$(tput cols)
+	TAB_SIZE=$((COLS / 5))
+	echo "initialized"
+}
+
+awake(){
+	echo "awake"
+	
+	welcomePrint
+}
+
+run(){
+	initialize
+	awake
+}
+
+
+printTab(){
+	local i=$TAB_SIZE
+	local s=" "
+	
+	if [ $# -eq 1 ]; then
+		i=$1;		
+	elif [ $# -ge 2 ]; then
+		i=$1;
+		s=$2;
+	fi
+	
+	while [ $i -gt 0 ]; do
+		printf "$s" $s;
+		i=$((i - 1));
+	done
+}
+
+welcomePrint(){
+	printTab $((COLS / 3))
+	echo $welcomeText
+}
+
+
+run
